da8cee19e91153789e391ba2204a87624852a70a
Piers Beasley <piers.beasley.19@ucl.ac.uk>
2021-09-22 11:20:01 +0100

    adds loading ref signal, received signal and deramping and decimation function

--- generic_scripts/bladeRAD_shell_script.sh
+++ generic_scripts/bladeRAD_shell_script.sh
@@ -12,43 +12,74 @@ trigger=$9
 clock=${10}
 tx_rx=${11}
 
+IFS=$''
+
 #Create trigger message
 if [ "$trigger" = 'master' ]
 then 
 	triggerctrl="j51-1";
 	fire="fire"
 	chain=$tx_rx
-	echo "Trigger Control : " $triggerctrl
 else 	
 	triggerctrl=""
 	fire=""
 	chain=""
-	echo "Trigger Control : " $triggerctrl
 fi	
 
 export triggerctrl
 
 #creatr clock ref message
-if [ $clock=1 ]; then clock_ref='set clock_sel external';
-elif [ $clock=2 ]; then clock_ref='set clock_out enable';
+if [ "$clock" = 1 ]; then clock_ref='set clock_sel external';
+elif [ "$clock" = 2 ]; then clock_ref='set clock_out enable';
 else clock_ref='set clock_ref enable'; fi
 
 
+
 # if Transmission required run the following
-if [ $tx_rx="tx" ]
+if [ "$tx_rx" = 'tx' ]
 then 
-	bladeRF-cli -d "*:serial=$sdr_serial" -e ' set frequency tx '$center_freq'M;
+	bladeRF-cli -d "*:serial=$sdr_serial" -e ' 
+			
+			set frequency tx '$center_freq'M;
 			set samplerate tx '$bw'M;
 			set bandwidth tx '$bw'M;
 			set gain tx1 '$tx_gain';
 
 			tx config file=/tmp/chirp.sc16q11 format=bin samples=16384 buffers=32 repeat='$pulses' timeout=30s;
 	
+    			'$clock_ref';
 			trigger j51-1 tx '$trigger';
-
+			print;
+            
+            
 			tx start;
 			trigger '$triggerctrl' '$chain' '$fire';
 			tx wait'
+fi
+
+
+
+# if Reception required run the following
+if [ "$tx_rx" = 'rx' ]
+then 
+	bladeRF-cli -d "*:serial=$sdr_serial" -e '
+			
+			set frequency rx '$center_freq'M;
+			set samplerate rx '$bw'M;
+			set bandwidth rx '$bw'M;
+			set agc off; 
+			set gain rx1'$rx_gain' ; 
+			
+			rx config file=/tmp/'$test_id'.sc16q11 format=bin n='$cap_samps' samples=16384 buffers=32 xfers=16 timeout=30s;
+			
+    			'$clock_ref';
+			trigger j51-1 rx '$trigger';
+			print;
+            
+            
+			rx start;
+			trigger '$triggerctrl' '$chain' '$fire';
+			rx wait'
 fi
 
 
