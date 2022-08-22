a5947f368f05a44bef65897154930e684b6b4ae4
Piers J Beasley <38111173+beasleyp@users.noreply.github.com>
2022-08-22 15:46:39 +0100

    adds CFAR slices

---
+++ generic_scripts/matlab/CFAR/cfarSlices.m
@@ -0,0 +1,28 @@
+function [cfar_range_doppler_slices] = cfarSlices(range_doppler_slices,detector,cutidx)
+%CFARSLICES loop through matrix and operate CFAR detector
+ 
+  number_cpi = size(range_doppler_slices,2); 
+  range_bins = size(range_doppler_slices{1},1);
+  doppler_bins = size(range_doppler_slices{1},2);
+  % CAF slices after CFAR (binary detections)
+    cfar_range_doppler_slices = createArrays(number_cpi, [range_bins doppler_bins]);         
+
+    for i=1:number_cpi-1
+        i
+        % CFAR the CAF to get detections array dets
+        [dets,th1] = detector(abs(range_doppler_slices{i}),cutidx);
+        % reshape array to matrix
+        caf = reshape(dets,[range_bins],[doppler_bins]);
+        cfar_range_doppler_slices{i} = caf;
+
+    end
+
+
+
+
+
+
+
+
+end
+
