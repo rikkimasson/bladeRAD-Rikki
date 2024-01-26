1a2794d56f4a5494e4e83d2238c0f98f14a8a2c8
JacobFrom <uceejpw@ucl.ac.uk>
2024-01-26 16:00:39 +0000

    Renamed Slice and processing
    Updated function for noise and renamed processing script and function to allow for merge.

--- generic_scripts/matlab/slice_SNR_branch.m
+++ generic_scripts/matlab/slice_SNR_branch.m
@@ -1,4 +1,4 @@
-function [signal, noise, SNR, slices_range_doppler, index_signal_max] = slice_SNR(range_doppler_slice)
+function [signal, noise, SNR, slices_range_doppler, index_signal_max] = slice_SNR(range_doppler_slice, roi_indices_rows, roi_indices_cols)
 
 %   This function takes a range doppler slice, a cell array containing
 %   complex numbers, uses the ABS function in MATLAB to calculate the complex
@@ -8,13 +8,18 @@ function [signal, noise, SNR, slices_range_doppler, index_signal_max] = slice_SN
 %   of all the numbers in the set).  The SNR is then calculated from these.
 %   All 3 are output.
 
+%Create region of interest matrix
+roi_data = range_doppler_slice(roi_indices_rows, roi_indices_cols);
+complex_2_abs_roi = abs(roi_data);
+
 complex_2_abs = abs(range_doppler_slice);
 abs_2_power = 20*log10(complex_2_abs);
 %signal = max(abs_2_power(:));
-[val, idx] = max(abs_2_power(:));
+[val, idx] = max(abs_2_power,[],"all");
 signal = val;
 index_signal_max = idx;
-noise = 20*log10(mean(complex_2_abs(:)));
+noise = 20*log10(mean(complex_2_abs_roi(:)));
+%noise = 20*log10(mean(complex_2_abs(:)));
 SNR = signal - noise;
 slices_range_doppler = abs_2_power;
 end
